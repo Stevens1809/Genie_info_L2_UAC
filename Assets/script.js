21f216086f599eaa8d192b38bf5d53093482ba12
arthurhekimamukirania@gmail.com <arthurmukiraniahekima@gmail.com>
2025-04-18 15:55:15 +0200

    reogarnisation du code et arrangement de la barre de menu avec du css

--- Assets/script.js
+++ Assets/script.js
@@ -9,9 +9,16 @@ function acceptCookies() {
     }
   };
 
-  const hamburger = document.getElementById("hamburger");
-  const nav = document.getElementById("nav");
-
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("active");
+  document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("menu").classList.toggle("active");
+  });
+  
+  document.querySelectorAll(".has-submenu > a").forEach((link) => {
+    link.addEventListener("click", function (e) {
+      if (window.innerWidth <= 768) {
+        e.preventDefault();
+        const parentLi = this.parentElement;
+        parentLi.classList.toggle("open");
+      }
+    });
   });
